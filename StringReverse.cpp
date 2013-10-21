ce9e49065c495b30b5ad2911e66544cd7cf03815
Tiranous <Tiranous@gimtaewon-ui-MacBook-Air.local>
2013-10-21 22:33:38 +0900

    modify Files...

--- StringReverse.cpp
+++ StringReverse.cpp
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Screen.h"
+
+void usage( void )
+{
+	fprintf(stdout, "Usage: StringReverse String\n");
+	fprintf(stdout, "Ex) StringReverse ABCDEFG\n");
+}
 
 int main( int argc, char** argv )
 {
 	/* Unused Variables */
-	( void ) argc; ( void ) argv;
+	//( void ) argc; ( void ) argv;
 
 	// Test Situation of Arguments.
 	//char bBuffer[] = "String Rerverse using Stack";
@@ -13,13 +20,23 @@ int main( int argc, char** argv )
 	//char* bBuffer = NULL;
 	//char bBuffer[] = "ÇÑ±Û TEST";
 	//char bBuffer[] = "-A";
+
+	if(argc < 2)
+	{
+		usage();
+		return -1;
+	}
+	
+	Screen( argv[1] );
 	
 	//while(true)
+	/*
 	{
 		//char bBuffer[] = "abcdefghijklmnopqrstuvwxyz";
 		char bBuffer[] = "123456789";
 		Screen( bBuffer );
 	}
+	*/
 	
 	return 0;
 }

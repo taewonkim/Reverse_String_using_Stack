28ee507a238da87c0c47328073c84a9a6bd1ece0
Taewon Kim <mniktw@gmail.com>
2019-11-05 21:58:07 +0900

    Update StringReverse.cpp

--- StringReverse.cpp
+++ StringReverse.cpp
@@ -1,42 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Screen.h"
 
-void usage( void )
+void usage( char* pName )
 {
-	fprintf(stdout, "Usage: StringReverse String\n");
-	fprintf(stdout, "Ex) StringReverse ABCDEFG\n");
+	if( pName == NULL )
+		return;
+	
+	fprintf(stdout, "Usage: %s String\n", pName);
+	fprintf(stdout, "Ex) %s ABCDEFG\n", pName);
 }
 
 int main( int argc, char** argv )
 {
-	/* Unused Variables */
-	//( void ) argc; ( void ) argv;
+	char* pBuffer = NULL;
+	unsigned int nLength = 0;
+	
+	/* UNUSED Variables */
+	/*
+		( void ) argc; ( void ) argv;
+	*/
 
 	// Test Situation of Arguments.
 	//char bBuffer[] = "String Rerverse using Stack";
 	//char bBuffer[] = "String";
 	//char bBuffer[] = "";
 	//char* bBuffer = NULL;
-	//char bBuffer[] = "�ѱ� TEST";
+	//char bBuffer[] = "ÇÑ±Û TEST";
 	//char bBuffer[] = "-A";
 
 	if(argc < 2)
 	{
-		usage();
+		usage( argv[0] );
 		return -1;
 	}
 	
-	Screen( argv[1] );
+	nLength = strlen( argv[1] );
+	pBuffer = (char *) malloc( nLength );
+	memset( pBuffer, 0, nLength );
+	strncpy( pBuffer, argv[1], nLength );
 	
-	//while(true)
-	/*
+	Screen( pBuffer );
+	
+	if( pBuffer != NULL )
 	{
-		//char bBuffer[] = "abcdefghijklmnopqrstuvwxyz";
-		char bBuffer[] = "123456789";
-		Screen( bBuffer );
+		free(pBuffer);
+		pBuffer = NULL;
 	}
-	*/
 	
 	return 0;
 }

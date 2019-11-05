b38f952912ba0012f06f59056006716b10080443
Taewon Kim <mniktw@gmail.com>
2019-11-05 21:45:54 +0900

    Update Screen.cpp

--- Screen.cpp
+++ Screen.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-#include "Const.h"
 #include "Reverse.h"
 
 void Screen( char* bBuffer )
 {
 	if( bBuffer != NULL )
 	{
-		fprintf(stdout, "Before: %s\n", bBuffer);
+		//fprintf(stdout, "Before: %s\n", bBuffer);
 		if( Reverse(bBuffer) == TRUE )
-			fprintf(stdout, "After : %s\n", bBuffer);
+			fprintf(stdout, "%s\n", bBuffer);
 	}
 }
